bd1c9855a0aacd039398b9e10f1f1eee96a70094
Carsten Brandt <mail@cebe.cc>
2017-01-26 10:49:50 +0100

    added missing dependencies

--- README.md
+++ README.md
@@ -16,3 +16,7 @@ The above packages are not available on Debian 8, you can install them anyway
 by running `sudo install-libclapack.sh`. This will load packages from Debian 9 which are compatible with Debian 8.
 
 cmake will patch and install pmvs. Patches are needed to compile it on a 64bit system.
+
+Further dependencies:
+
+    apt-get install libgsl0-dev
--- install-libclapack.sh
+++ install-libclapack.sh
@@ -5,7 +5,7 @@ PACKAGES="$PACKAGES http://ftp.de.debian.org/debian/pool/main/c/clapack/libclapa
 PACKAGES="$PACKAGES http://ftp.de.debian.org/debian/pool/main/c/clapack/libclapack3_3.2.1+dfsg-1_amd64.deb"
 PACKAGES="$PACKAGES http://ftp.de.debian.org/debian/pool/main/c/clapack/libcblas3_3.2.1+dfsg-1_amd64.deb"
 PACKAGES="$PACKAGES http://ftp.de.debian.org/debian/pool/main/l/lapack/libblas-common_3.7.0-1_amd64.deb"
-PACKAGES="$PACKAGES http://ftp.de.debian.org/debian/pool/main/libf/libf2c2/libf2c2-dev_20130926-2_amd64.deb"
+#PACKAGES="$PACKAGES http://ftp.de.debian.org/debian/pool/main/libf/libf2c2/libf2c2-dev_20130926-2_amd64.deb"
 
 sudo apt-get install libf2c2 libf2c2-dev
 
