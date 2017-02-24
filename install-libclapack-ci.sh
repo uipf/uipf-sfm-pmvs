77768136c7244558464435bde340e5efb7412da6
Carsten Brandt <mail@cebe.cc>
2017-02-24 13:48:03 +0100

    more compatible way of installing lapack

--- install-libclapack-ci.sh
+++ install-libclapack-ci.sh
@@ -2,18 +2,25 @@
 
 # this version of install-libclapack.sh is used for building packages in CI environment.
 
-PACKAGES=""
-PACKAGES="$PACKAGES http://ftp.de.debian.org/debian/pool/main/c/clapack/libclapack-dev_3.2.1+dfsg-1_amd64.deb"
-PACKAGES="$PACKAGES http://ftp.de.debian.org/debian/pool/main/c/clapack/libclapack3_3.2.1+dfsg-1_amd64.deb"
-PACKAGES="$PACKAGES http://ftp.de.debian.org/debian/pool/main/c/clapack/libcblas3_3.2.1+dfsg-1_amd64.deb"
-PACKAGES="$PACKAGES http://ftp.de.debian.org/debian/pool/main/l/lapack/libblas-common_3.7.0-1_amd64.deb"
-#PACKAGES="$PACKAGES http://ftp.de.debian.org/debian/pool/main/libf/libf2c2/libf2c2-dev_20130926-2_amd64.deb"
+APTINSTALL=
+if apt-cache pkgnames |grep libclapack-dev ; then
+    APTINSTALL="${APTINSTALL} libclapack-dev"
+else
+    wget http://ftp.de.debian.org/debian/pool/main/c/clapack/libclapack-dev_3.2.1+dfsg-1_amd64.deb
+fi
 
-apt-get -y install libf2c2 libf2c2-dev
+if apt-cache pkgnames |grep libcblas3 ; then
+    APTINSTALL="${APTINSTALL} libcblas3"
+else
+    wget http://ftp.de.debian.org/debian/pool/main/c/clapack/libcblas3_3.2.1+dfsg-1_amd64.deb
+fi
 
-for p in $PACKAGES
-do
-	wget $p
-done
+if apt-cache pkgnames |grep libblas-common ; then
+    APTINSTALL="${APTINSTALL} libblas-common"
+else
+    wget http://ftp.de.debian.org/debian/pool/main/l/lapack/libblas-common_3.7.0-1_amd64.deb
+fi
 
-dpkg -i *.deb
+apt-get -y --no-install-recommends install $APTINSTALL libf2c2 libf2c2-dev libgsl0-dev
+
+ls *.deb && dpkg -i *.deb || echo "ok"
