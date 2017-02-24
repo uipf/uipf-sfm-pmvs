3fddde73438732a3f138daec5013236d01526ffb
Carsten Brandt <mail@cebe.cc>
2017-02-24 21:16:39 +0100

    update packages for ubuntu

--- install-libclapack-ci.sh
+++ install-libclapack-ci.sh
@@ -8,25 +8,41 @@ APTINSTALL=
 if apt-cache pkgnames |grep libclapack-dev ; then
     APTINSTALL="${APTINSTALL} libclapack3"
 else
-    wget  http://ftp.de.debian.org/debian/pool/main/c/clapack/libclapack3_3.2.1+dfsg-1_amd64.deb
+    if [ "$(lsb_release -is)" = "Ubuntu" ] ; then
+        wget  http://de.archive.ubuntu.com/ubuntu/pool/universe/c/clapack/libclapack3_3.2.1+dfsg-1_amd64.deb
+    else
+        wget  http://ftp.de.debian.org/debian/pool/main/c/clapack/libclapack3_3.2.1+dfsg-1_amd64.deb
+    fi
 fi
 
 if apt-cache pkgnames |grep libclapack-dev ; then
     APTINSTALL="${APTINSTALL} libclapack-dev"
 else
-    wget http://ftp.de.debian.org/debian/pool/main/c/clapack/libclapack-dev_3.2.1+dfsg-1_amd64.deb
+    if [ "$(lsb_release -is)" = "Ubuntu" ] ; then
+        wget  http://de.archive.ubuntu.com/ubuntu/pool/universe/c/clapack/libclapack-dev_3.2.1+dfsg-1_amd64.deb
+    else
+        wget http://ftp.de.debian.org/debian/pool/main/c/clapack/libclapack-dev_3.2.1+dfsg-1_amd64.deb
+    fi
 fi
 
 if apt-cache pkgnames |grep libcblas3 ; then
     APTINSTALL="${APTINSTALL} libcblas3"
 else
-    wget http://ftp.de.debian.org/debian/pool/main/c/clapack/libcblas3_3.2.1+dfsg-1_amd64.deb
+    if [ "$(lsb_release -is)" = "Ubuntu" ] ; then
+        wget http://de.archive.ubuntu.com/ubuntu/pool/universe/c/clapack/libcblas3_3.2.1+dfsg-1_amd64.deb
+    else
+        wget http://ftp.de.debian.org/debian/pool/main/c/clapack/libcblas3_3.2.1+dfsg-1_amd64.deb
+    fi
 fi
 
 if apt-cache pkgnames |grep libblas-common ; then
     APTINSTALL="${APTINSTALL} libblas-common"
 else
-    wget http://ftp.de.debian.org/debian/pool/main/l/lapack/libblas-common_3.7.0-1_amd64.deb
+    if [ "$(lsb_release -is)" = "Ubuntu" ] ; then
+        wget http://de.archive.ubuntu.com/ubuntu/pool/main/l/lapack/libblas-common_3.6.0-2ubuntu2_amd64.deb
+    else
+        wget http://ftp.de.debian.org/debian/pool/main/l/lapack/libblas-common_3.7.0-1_amd64.deb
+    fi
 fi
 
 sudo apt-get -y --no-install-recommends install $APTINSTALL libf2c2 libf2c2-dev libgsl0-dev
