77bccfceaf8c80fc898ce91926801fd70a022385
Carsten Brandt <mail@cebe.cc>
2017-02-24 21:55:53 +0100

    proper backports on ubuntu

--- install-libclapack-ci.sh
+++ install-libclapack-ci.sh
@@ -5,11 +5,12 @@
 which sudo || apt-get -y install sudo
 
 APTINSTALL=
+PBUILDER=
 if apt-cache pkgnames |grep libclapack-dev ; then
     APTINSTALL="${APTINSTALL} libclapack3"
 else
     if [ "$(lsb_release -is)" = "Ubuntu" ] ; then
-        wget  http://de.archive.ubuntu.com/ubuntu/pool/universe/c/clapack/libclapack3_3.2.1+dfsg-1_amd64.deb
+        PBUILDER="${PBUILDER} libclapack3"
     else
         wget  http://ftp.de.debian.org/debian/pool/main/c/clapack/libclapack3_3.2.1+dfsg-1_amd64.deb
     fi
@@ -19,7 +20,7 @@ if apt-cache pkgnames |grep libclapack-dev ; then
     APTINSTALL="${APTINSTALL} libclapack-dev"
 else
     if [ "$(lsb_release -is)" = "Ubuntu" ] ; then
-        wget  http://de.archive.ubuntu.com/ubuntu/pool/universe/c/clapack/libclapack-dev_3.2.1+dfsg-1_amd64.deb
+        PBUILDER="${PBUILDER} libclapack-dev"
     else
         wget http://ftp.de.debian.org/debian/pool/main/c/clapack/libclapack-dev_3.2.1+dfsg-1_amd64.deb
     fi
@@ -29,7 +30,7 @@ if apt-cache pkgnames |grep libcblas3 ; then
     APTINSTALL="${APTINSTALL} libcblas3"
 else
     if [ "$(lsb_release -is)" = "Ubuntu" ] ; then
-        wget http://de.archive.ubuntu.com/ubuntu/pool/universe/c/clapack/libcblas3_3.2.1+dfsg-1_amd64.deb
+        PBUILDER="${PBUILDER} libcblas3"
     else
         wget http://ftp.de.debian.org/debian/pool/main/c/clapack/libcblas3_3.2.1+dfsg-1_amd64.deb
     fi
@@ -39,12 +40,22 @@ if apt-cache pkgnames |grep libblas-common ; then
     APTINSTALL="${APTINSTALL} libblas-common"
 else
     if [ "$(lsb_release -is)" = "Ubuntu" ] ; then
-        wget http://de.archive.ubuntu.com/ubuntu/pool/main/l/lapack/libblas-common_3.6.0-2ubuntu2_amd64.deb
+        PBUILDER="${PBUILDER} libblas-common"
     else
         wget http://ftp.de.debian.org/debian/pool/main/l/lapack/libblas-common_3.7.0-1_amd64.deb
     fi
 fi
 
 sudo apt-get -y --no-install-recommends install $APTINSTALL libf2c2 libf2c2-dev libgsl0-dev
+
+if [ "$PBUILDER" != "" ] ; then
+
+    apt-get source $PBUILDER
+    sudo apt-get install pbuilder debootstrap devscripts
+    sudo pbuilder create --debootstrapopts --variant=buildd
+    sudo pbuilder build *.dsc
+    cp /var/cache/pbuilder/result/*.deb .
+
+fi
 
 ls *.deb && sudo dpkg -i *.deb || echo "ok"
